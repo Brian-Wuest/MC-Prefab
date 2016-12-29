32fbd44fe9c10d3436f2582a2dd15ef38cfe23b8
WuestMan <redknightofdoom@yahoo.com>
2016-12-28 23:04:54 -0600

    Allowing chicken coop to spawn with chicken.

--- src/main/java/com/wuest/prefab/StructureGen/CustomStructures/StructureChickenCoop.java
+++ src/main/java/com/wuest/prefab/StructureGen/CustomStructures/StructureChickenCoop.java
@@ -1,8 +1,23 @@
 package com.wuest.prefab.StructureGen.CustomStructures;
 
+import com.wuest.prefab.Config.BasicStructureConfiguration;
+import com.wuest.prefab.Config.StructureConfiguration;
+import com.wuest.prefab.Config.BasicStructureConfiguration.EnumBasicStructureName;
+import com.wuest.prefab.StructureGen.BuildBlock;
 import com.wuest.prefab.StructureGen.BuildClear;
 import com.wuest.prefab.StructureGen.Structure;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockFenceGate;
+import net.minecraft.block.BlockHalfWoodSlab;
+import net.minecraft.block.BlockHay;
+import net.minecraft.block.BlockHopper;
+import net.minecraft.block.BlockSlab;
+import net.minecraft.block.BlockSlab.EnumBlockHalf;
+import net.minecraft.block.BlockWoodSlab;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -10,7 +25,9 @@ import net.minecraft.world.World;
 public class StructureChickenCoop extends Structure
 {
 	public static final String ASSETLOCATION = "assets/prefab/structures/chickencoop.zip";
-
+	BlockPos chickenCoopBlockPos = null;
+	
+	
 	public static void ScanStructure(World world, BlockPos originalPos, EnumFacing playerFacing)
 	{
 		BuildClear clearedSpace = new BuildClear();
@@ -29,5 +46,44 @@ public class StructureChickenCoop extends Structure
 				"C:\\Users\\Brian\\Documents\\GitHub\\MC-Prefab\\src\\main\\resources\\assets\\prefab\\structures\\chickencoop.zip",
 				clearedSpace,
 				playerFacing);	
+	}
+	
+	@Override
+	protected Boolean CustomBlockProcessingHandled(StructureConfiguration configuration, BuildBlock block, World world, BlockPos originalPos,
+			EnumFacing assumedNorth, Block foundBlock, IBlockState blockState, EntityPlayer player)
+	{
+		if (foundBlock instanceof BlockFenceGate && this.chickenCoopBlockPos == null)
+		{ 
+			chickenCoopBlockPos = block.getStartingPosition().getRelativePosition(originalPos, configuration.houseFacing).offset(configuration.houseFacing.getOpposite(), 2);
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * This method is used after the main building is build for any additional
+	 * structures or modifications.
+	 * 
+	 * @param configuration The structure configuration.
+	 * @param world The current world.
+	 * @param originalPos The original position clicked on.
+	 * @param assumedNorth The assumed northern direction.
+	 * @param player The player which initiated the construction.
+	 */
+	@Override
+	public void AfterBuilding(StructureConfiguration configuration, World world, BlockPos originalPos, EnumFacing assumedNorth, EntityPlayer player)
+	{
+		if (this.chickenCoopBlockPos != null)
+		{
+			// For the chicken coop, spawn 1 chicken above a hay bale.
+			for (int i = 0; i < 1; i++)
+			{
+				EntityChicken entity = new EntityChicken(world);
+				entity.setPosition(this.chickenCoopBlockPos.getX(), this.chickenCoopBlockPos.getY(), this.chickenCoopBlockPos.getZ());
+				world.spawnEntityInWorld(entity);
+			}
+			
+			this.chickenCoopBlockPos = null;
+		}
 	}
 }
