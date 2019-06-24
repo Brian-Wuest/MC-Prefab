786963388d4a3593f3d3d782a553fccffd56f6ac
Brian Wuest <brian.wuest@outlook.com>
2019-06-24 17:03:15 -0500

    Capabilitiy stuff and removing un-used items.

--- src/main/java/com/wuest/prefab/Structures/Capabilities/StructureConfigurationProvider.java
+++ src/main/java/com/wuest/prefab/Structures/Capabilities/StructureConfigurationProvider.java
@@ -1,50 +1,53 @@
 package com.wuest.prefab.Structures.Capabilities;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import com.wuest.prefab.ModRegistry;
 
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.util.LazyOptional;
 
 /**
  * This is the provider class for the structure configuration capability.
+ * 
  * @author WuestMan
  *
  */
-public class StructureConfigurationProvider implements ICapabilitySerializable<NBTTagCompound>
+public class StructureConfigurationProvider implements ICapabilitySerializable<CompoundNBT>
 {
 	private IStructureConfigurationCapability structureConfigurationCapability;
-	
+
 	/**
 	 * Initializes a new instance of the StructureConfigurationProvider class.
+	 * 
 	 * @param structureConfigurationCapability The {@link IStructureConfigurationCapability} to be used by this class.
 	 */
 	public StructureConfigurationProvider(IStructureConfigurationCapability structureConfigurationCapability)
 	{
 		this.structureConfigurationCapability = structureConfigurationCapability;
 	}
-	
+
 	@Override
-	public boolean hasCapability(Capability<?> capability, EnumFacing facing)
+	@Nonnull
+	public <T> LazyOptional<T> getCapability(@Nonnull final Capability<T> cap, final @Nullable Direction side)
 	{
-		return capability == ModRegistry.StructureConfiguration;
+		LazyOptional<IStructureConfigurationCapability> optionalOfCurrentCapability = LazyOptional.of(() -> structureConfigurationCapability);
+		
+		return cap == ModRegistry.StructureConfiguration ? optionalOfCurrentCapability.cast() : LazyOptional.empty();
 	}
 
 	@Override
-	public <T> T getCapability(Capability<T> capability, EnumFacing facing)
+	public CompoundNBT serializeNBT()
 	{
-		return capability == ModRegistry.StructureConfiguration ? ModRegistry.StructureConfiguration.cast(this.structureConfigurationCapability) : null;
+		return (CompoundNBT) ModRegistry.StructureConfiguration.getStorage().writeNBT(ModRegistry.StructureConfiguration, this.structureConfigurationCapability, null);
 	}
 
 	@Override
-	public NBTTagCompound serializeNBT()
-	{
-		return (NBTTagCompound)ModRegistry.StructureConfiguration.getStorage().writeNBT(ModRegistry.StructureConfiguration, this.structureConfigurationCapability, null);
-	}
-
-	@Override
-	public void deserializeNBT(NBTTagCompound nbt)
+	public void deserializeNBT(CompoundNBT nbt)
 	{
 		ModRegistry.StructureConfiguration.getStorage().readNBT(ModRegistry.StructureConfiguration, this.structureConfigurationCapability, null, nbt);
 	}
