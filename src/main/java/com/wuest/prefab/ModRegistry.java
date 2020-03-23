9f741eaee694f8c6c3f8a976d5b5d5538e95e343
Brian Wuest <brian.wuest@outlook.com>
2020-03-23 18:26:02 -0500

    Fixing some import issues and updating required forge version

--- src/main/java/com/wuest/prefab/ModRegistry.java
+++ src/main/java/com/wuest/prefab/ModRegistry.java
@@ -1,8 +1,6 @@
 package com.wuest.prefab;
 
-import com.wuest.prefab.Blocks.BlockCompressedObsidian;
-import com.wuest.prefab.Blocks.BlockCompressedStone;
-import com.wuest.prefab.Blocks.BlockStairs;
+import com.wuest.prefab.Blocks.*;
 import com.wuest.prefab.Items.*;
 import com.wuest.prefab.Proxy.Messages.ConfigSyncMessage;
 import com.wuest.prefab.Proxy.Messages.Handlers.ConfigSyncHandler;
