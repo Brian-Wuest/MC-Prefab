cc5785dd8eb8aac62d21c0c938deee4353e7241b
Brian Wuest <brian.wuest@outlook.com>
2021-06-14 19:13:46 -0500

    Fixing issue where the advanced warehouse was generating the basic warehouse.

--- src/main/java/com/wuest/prefab/structures/items/ItemAdvancedWareHouse.java
+++ src/main/java/com/wuest/prefab/structures/items/ItemAdvancedWareHouse.java
@@ -1,5 +1,7 @@
 package com.wuest.prefab.structures.items;
 
+import com.wuest.prefab.Prefab;
+import com.wuest.prefab.structures.gui.GuiAdvancedWareHouse;
 import com.wuest.prefab.structures.predefined.StructureWarehouse;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumHand;
@@ -12,6 +14,16 @@ import net.minecraft.world.World;
 public class ItemAdvancedWareHouse extends ItemWareHouse {
     public ItemAdvancedWareHouse(String name) {
         super(name);
+    }
+
+    /**
+     * Initializes common fields/properties for this structure item.
+     */
+    @Override
+    protected void PostInit() {
+        if (Prefab.proxy.isClient) {
+            this.RegisterGui(GuiAdvancedWareHouse.class);
+        }
     }
 
     @Override
