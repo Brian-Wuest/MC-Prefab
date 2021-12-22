15e869115a89704b1da501ffa0465ba0b7bbc717
Brian Wuest <brian.wuest@outlook.com>
2021-12-22 07:33:59 -0600

    Fixing issues with the monster masher. Closes #240.

--- src/main/java/com/wuest/prefab/structures/predefined/StructureBasic.java
+++ src/main/java/com/wuest/prefab/structures/predefined/StructureBasic.java
@@ -1,21 +1,29 @@
 package com.wuest.prefab.structures.predefined;
 
+import com.wuest.prefab.Prefab;
 import com.wuest.prefab.Tuple;
-import com.wuest.prefab.structures.base.*;
+import com.wuest.prefab.structures.base.BuildBlock;
+import com.wuest.prefab.structures.base.BuildingMethods;
+import com.wuest.prefab.structures.base.Structure;
 import com.wuest.prefab.structures.config.BasicStructureConfiguration;
 import com.wuest.prefab.structures.config.BasicStructureConfiguration.EnumBasicStructureName;
 import com.wuest.prefab.structures.config.StructureConfiguration;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockBed;
-import net.minecraft.block.BlockHopper;
-import net.minecraft.block.BlockTrapDoor;
+import net.minecraft.block.*;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.EntitySpider;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityMobSpawner;
+import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
@@ -26,25 +34,28 @@ import java.util.ArrayList;
  * @author WuestMan
  */
 public class StructureBasic extends Structure {
-    BlockPos customBlockPos = null;
     private final ArrayList<Tuple<BlockPos, BlockPos>> bedPositions = new ArrayList<>();
+    private final ArrayList<BlockPos> mobSpawnerPos = new ArrayList<>();
+    BlockPos customBlockPos = null;
+    private BlockPos signPosition = null;
 
     @Override
     protected Boolean CustomBlockProcessingHandled(StructureConfiguration configuration, BuildBlock block, World world, BlockPos originalPos,
                                                    EnumFacing assumedNorth, Block foundBlock, IBlockState blockState, EntityPlayer player) {
         BasicStructureConfiguration config = (BasicStructureConfiguration) configuration;
+        EnumBasicStructureName enumName = config.basicStructureName;
 
-        if (foundBlock instanceof BlockHopper && config.basicStructureName.getName().equals(EnumBasicStructureName.AdvancedCoop.getName())) {
+        if (foundBlock instanceof BlockHopper && enumName.getName().equals(EnumBasicStructureName.AdvancedCoop.getName())) {
             customBlockPos = block.getStartingPosition().getRelativePosition(
                     originalPos,
                     this.getClearSpace().getShape().getDirection(),
                     configuration.houseFacing);
-        } else if (foundBlock instanceof BlockTrapDoor && config.basicStructureName.getName().equals(EnumBasicStructureName.MineshaftEntrance.getName())) {
+        } else if (foundBlock instanceof BlockTrapDoor && enumName.getName().equals(EnumBasicStructureName.MineshaftEntrance.getName())) {
             customBlockPos = block.getStartingPosition().getRelativePosition(
                     originalPos,
                     this.getClearSpace().getShape().getDirection(),
                     configuration.houseFacing);
-        }  else if (foundBlock instanceof BlockBed && config.chosenOption.getHasBedColor()) {
+        } else if (foundBlock instanceof BlockBed && config.chosenOption.getHasBedColor()) {
             // Even if a structure has a bed; we may want to keep a specific color to match what the design of the structure is.
             BlockPos bedHeadPosition = block.getStartingPosition().getRelativePosition(originalPos, this.getClearSpace().getShape().getDirection(), configuration.houseFacing);
             BlockPos bedFootPosition = block.getSubBlock().getStartingPosition().getRelativePosition(
@@ -55,6 +66,17 @@ public class StructureBasic extends Structure {
             this.bedPositions.add(new Tuple<>(bedHeadPosition, bedFootPosition));
 
             return true;
+        } else if (foundBlock instanceof BlockMobSpawner && enumName.getName().equals(EnumBasicStructureName.MonsterMasher.getName())
+                && Prefab.proxy.getServerConfiguration().includeSpawnersInMasher) {
+            this.mobSpawnerPos.add(block.getStartingPosition().getRelativePosition(
+                    originalPos,
+                    this.getClearSpace().getShape().getDirection(),
+                    configuration.houseFacing));
+        } else if (foundBlock instanceof BlockSign && enumName.getName().equals(EnumBasicStructureName.MonsterMasher.getName())) {
+            this.signPosition = block.getStartingPosition().getRelativePosition(
+                    originalPos,
+                    this.getClearSpace().getShape().getDirection(),
+                    configuration.houseFacing);
         }
 
         if (foundBlock.getRegistryName().getNamespace().equals(Blocks.STAINED_GLASS.getRegistryName().getNamespace())
@@ -65,7 +87,7 @@ public class StructureBasic extends Structure {
             this.priorityOneBlocks.add(block);
 
             return true;
-        } else         if (foundBlock.getRegistryName().getNamespace().equals(Blocks.STAINED_GLASS_PANE.getRegistryName().getNamespace())
+        } else if (foundBlock.getRegistryName().getNamespace().equals(Blocks.STAINED_GLASS_PANE.getRegistryName().getNamespace())
                 && foundBlock.getRegistryName().getPath().equals(Blocks.STAINED_GLASS_PANE.getRegistryName().getPath())
                 && config.chosenOption.getHasGlassColor()) {
             blockState = this.getStainedGlassPaneBlock(config.glassColor);
@@ -100,26 +122,79 @@ public class StructureBasic extends Structure {
     @Override
     public void AfterBuilding(StructureConfiguration configuration, World world, BlockPos originalPos, EnumFacing assumedNorth, EntityPlayer player) {
         BasicStructureConfiguration config = (BasicStructureConfiguration) configuration;
+        EnumBasicStructureName enumName = config.basicStructureName;
 
         if (this.customBlockPos != null) {
-            if (config.basicStructureName.getName().equals(EnumBasicStructureName.AdvancedCoop.getName())) {
+            if (enumName.getName().equals(EnumBasicStructureName.AdvancedCoop.getName())) {
                 // For the advanced chicken coop, spawn 4 chickens above the hopper.
                 for (int i = 0; i < 4; i++) {
                     EntityChicken entity = new EntityChicken(world);
                     entity.setPosition(this.customBlockPos.getX(), this.customBlockPos.up().getY(), this.customBlockPos.getZ());
                     world.spawnEntity(entity);
                 }
-            } else if (config.basicStructureName.getName().equals(EnumBasicStructureName.ChickenCoop.getName())) {
+            } else if (enumName.getName().equals(EnumBasicStructureName.ChickenCoop.getName())) {
                 // For the chicken coop, spawn 4 chickens above the hopper.
                 EntityChicken entity = new EntityChicken(world);
                 entity.setPosition(this.customBlockPos.getX(), this.customBlockPos.down().getY(), this.customBlockPos.getZ());
                 world.spawnEntity(entity);
-            } else if (config.basicStructureName.getName().equals(EnumBasicStructureName.MineshaftEntrance.getName())) {
+            } else if (enumName.getName().equals(EnumBasicStructureName.MineshaftEntrance.getName())) {
                 // Build the mineshaft where the trap door exists.
                 StructureAlternateStart.PlaceMineShaft(world, this.customBlockPos.down(), configuration.houseFacing, true);
             }
 
             this.customBlockPos = null;
+        }
+
+        if (enumName.getName().equals(EnumBasicStructureName.MonsterMasher.getName())) {
+            int monstersPlaced = 0;
+
+            // Set the spawner.
+            for (BlockPos pos : this.mobSpawnerPos) {
+                TileEntity tileEntity = world.getTileEntity(pos);
+
+                if (tileEntity instanceof TileEntityMobSpawner) {
+                    TileEntityMobSpawner spawner = (TileEntityMobSpawner) tileEntity;
+
+                    switch (monstersPlaced) {
+                        case 0: {
+                            // Zombie.
+                            spawner.getSpawnerBaseLogic().setEntityId(EntityList.getKey(EntityZombie.class));
+                            break;
+                        }
+
+                        case 1: {
+                            // Skeleton.
+                            spawner.getSpawnerBaseLogic().setEntityId(EntityList.getKey(EntitySkeleton.class));
+                            break;
+                        }
+
+                        case 2: {
+                            // Spider.
+                            spawner.getSpawnerBaseLogic().setEntityId(EntityList.getKey(EntitySpider.class));
+                            break;
+                        }
+
+                        default: {
+                            // Creeper.
+                            spawner.getSpawnerBaseLogic().setEntityId(EntityList.getKey(EntityCreeper.class));
+                            break;
+                        }
+                    }
+
+                    monstersPlaced++;
+                }
+            }
+
+            if (this.signPosition != null) {
+                TileEntity tileEntity = world.getTileEntity(this.signPosition);
+
+                if (tileEntity instanceof TileEntitySign) {
+                    TileEntitySign signTile = (TileEntitySign) tileEntity;
+                    signTile.signText[0] = new TextComponentString("Lamp On=Mobs");
+
+                    signTile.signText[2] = new TextComponentString("Lamp Off=No Mobs");
+                }
+            }
         }
 
         if (this.bedPositions.size() > 0) {
