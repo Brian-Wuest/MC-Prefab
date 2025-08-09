af56206ef5805750c9b73469e4e9247725f4563b
Brian Wuest <brian.wuest@outlook.com>
2025-08-09 09:27:23 -0500

    Fabric: Updating canBreakBlock event to match NeoForge

--- Fabric/src/main/java/com/prefab/fabric/EventCaller.java
+++ Fabric/src/main/java/com/prefab/fabric/EventCaller.java
@@ -4,13 +4,25 @@ import com.prefab.IEventCaller;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.block.GameMasterBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class EventCaller implements IEventCaller {
 
     @Override
     public boolean canBreakBlock(ServerLevel world, Player player, BlockState blockState, BlockPos blockPos) {
+        GameType gameType = ((ServerPlayer)player).gameMode.getGameModeForPlayer();
+        if (player.blockActionRestricted(world, blockPos, gameType)) {
+            return false;
+        }
+
+        if (blockState.getBlock() instanceof GameMasterBlock && !player.canUseGameMasterBlocks()) {
+            return false;
+        }
+
         return PlayerBlockBreakEvents.BEFORE.invoker().beforeBlockBreak(world, player, blockPos, blockState, null);
     }
 }
