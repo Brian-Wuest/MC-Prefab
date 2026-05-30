69d281d83a9a588062984552a86867cd56843d51
Brian Wuest <brian.wuest@outlook.com>
2026-05-30 10:19:46 -0500

    Adding base for data generation for the future

---
+++ Fabric/src/main/java/com/prefab/fabric/PrefabDocsReferenceDataGenerator.java
@@ -0,0 +1,12 @@
+package com.prefab.fabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PrefabDocsReferenceDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+
+}
