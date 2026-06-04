1d61189b608deb2222efbe4af89893112a40c0f8
Brian Wuest <brian.wuest@outlook.com>
2026-06-04 06:23:14 -0500

    Cleaning up rendering class
    Also making transparency better by making it more transparent with the new rendering process.

--- Shared/src/com/prefab/PrefabClientBase.java
+++ Shared/src/com/prefab/PrefabClientBase.java
@@ -4,7 +4,9 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.Util;
-import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.CoreShaders;
+import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.TriState;
@@ -14,7 +16,6 @@ import java.util.function.Function;
 import static net.minecraft.client.renderer.RenderStateShard.*;
 
 public class PrefabClientBase {
-
     public static final RenderStateShard.ShaderStateShard RENDER_TYPE_ENTITY_TRANSLUCENT_CULL_SHADER = new RenderStateShard.ShaderStateShard(
             CoreShaders.RENDERTYPE_ENTITY_TRANSLUCENT);
 
@@ -34,48 +35,11 @@ public class PrefabClientBase {
                         1536, true, true, rendertype$compositestate);
             }
     );
+    public static final RenderType PREVIEW_LAYER_2 = new PreviewLayer();
 
     public static final RenderType getEntityTranslucentCull() {
         return ENTITY_TRANSLUCENT_CULL.apply(TextureAtlas.LOCATION_BLOCKS);
     }
-
-    /*public static final RenderType PREVIEW_LAYER_2 = RenderType.create(        "ghost_preview_shimmer",
-            DefaultVertexFormat.NEW_ENTITY,
-            VertexFormat.Mode.QUADS,
-            262144,
-            false,
-            true,
-            RenderType.CompositeState.builder()
-                    .setShaderState(new RenderStateShard.ShaderStateShard(() -> GhostShaders.GHOST_SHIMMER_SHADER))
-                    .setTextureState(new RenderStateShard.TextureStateShard(TextureAtlas.LOCATION_BLOCKS, false, false))
-                    .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                    .setDepthTestState(RenderStateShard.LEQUAL_DEPTH_TEST)   // ✔ depth test ON
-                    .setWriteMaskState(RenderStateShard.COLOR_WRITE)         // ✔ depth write OFF
-                    .setCullState(RenderStateShard.NO_CULL)                  // ✔ show all faces
-                    .setLightmapState(RenderStateShard.LIGHTMAP)
-                    .setOverlayState(RenderStateShard.OVERLAY)
-                    .createCompositeState(false)
-    );*/
-
-    /*public static class PreviewLayer2 extends RenderType {
-        public PreviewLayer2() {
-            super(PrefabBase.MODID + ".ghost_preview_shimmer", DefaultVertexFormat.NEW_ENTITY,
-                    VertexFormat.Mode.QUADS,
-                    786432, false,
-                    false,
-                    () -> {
-                        Sheets.translucentCullBlockSheet().setupRenderState();
-                        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 0.80F);
-                    }, () -> {
-                        Sheets.translucentCullBlockSheet().clearRenderState();
-                        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-                    });
-        }
-    }*/
-
-    /*public static final RenderType PREVIEW_LAYER = new PreviewLayer();*/
-
-    public static final RenderType PREVIEW_LAYER_2 = new PreviewLayer();
 
     public static class PreviewLayer extends RenderType {
         public PreviewLayer() {
@@ -85,7 +49,7 @@ public class PrefabClientBase {
                     false,
                     () -> {
                         getEntityTranslucentCull().setupRenderState();
-                        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 0.87F);
+                        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 0.77F);
                     }, () -> {
                         getEntityTranslucentCull().clearRenderState();
                         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
