c6afd73fd3c293e957c424b4c37b3bd08c51bcb1
Brian Wuest <brian.wuest@outlook.com>
2026-08-05 06:14:19 -0500

    Fabric: Fixing ModMenu integration warning

--- Fabric/src/main/java/com/prefab/fabric/config/PrefabModMenuIntegration.java
+++ Fabric/src/main/java/com/prefab/fabric/config/PrefabModMenuIntegration.java
@@ -3,7 +3,7 @@ package com.prefab.fabric.config;
 import com.prefab.config.ModConfiguration;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
@@ -12,6 +12,6 @@ public class PrefabModMenuIntegration implements ModMenuApi {
 
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return parent -> AutoConfig.getConfigScreen(ModConfiguration.class, parent).get();
+		return parent -> AutoConfigClient.getConfigScreen(ModConfiguration.class, parent).get();
 	}
 }
