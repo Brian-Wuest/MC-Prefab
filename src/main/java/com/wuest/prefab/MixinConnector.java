9f1b117fb1ee9ea957bcab78a4f4a4fedd1f298d
Brian Wuest <brian.wuest@outlook.com>
2026-08-09 16:26:10 -0500

    Update to latest forge
    Also fixing build system as a result of the update

--- src/main/java/com/wuest/prefab/MixinConnector.java
+++ src/main/java/com/wuest/prefab/MixinConnector.java
@@ -6,6 +6,6 @@ import org.spongepowered.asm.mixin.connect.IMixinConnector;
 public class MixinConnector implements IMixinConnector {
     @Override
     public void connect() {
-        Mixins.addConfigurations("assets/prefab/prefab.mixins.json");
+        Mixins.addConfigurations("/assets/prefab/prefab.mixins.json");
     }
 }
